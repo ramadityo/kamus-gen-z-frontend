a8a5de852b3bc06c99ccf31ffe32f2e8c460b324
_userNull <ramzprivatexd@gmail.com>
2024-11-12 21:31:21 +0700

    add catppuccin theme

---
+++ src/font/fonts.js
@@ -0,0 +1,1 @@
+import { Noto_Sans } from "next/font/google";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                background: "#FBF6F8",
+                // yellow: "#e5c890",
+                // red: "#e78284",
+                // teal: "#81c8be",
+                // sky: "#99d1db",
+                // sapphire: "#85c1dc",
+                // blue: "#8caaee",
+                // crust: "#232634",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        require("@catppuccin/tailwindcss")({
+            defaultFlavour: "frappe",
+        }),
+    ],
 };
