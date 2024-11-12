e2cdf687a96c04ec2d4f91bfa58cfc1310aaa341
_userNull <ramzprivatexd@gmail.com>
2024-11-12 22:14:44 +0700

    add fonts

--- src/font/fonts.js
+++ src/font/fonts.js
@@ -1,1 +1,20 @@
 import { Noto_Sans } from "next/font/google";
+
+const NotoSansReg = Noto_Sans({
+    weight: "400",
+    subsets: ["latin"],
+});
+const NotoSansMed = Noto_Sans({
+    weight: "500",
+    subsets: ["latin"],
+});
+const NotoSansSemBold = Noto_Sans({
+    weight: "600",
+    subsets: ["latin"],
+});
+const NotoSansBold = Noto_Sans({
+    weight: "700",
+    subsets: ["latin"],
+});
+
+export default { NotoSansReg, NotoSansMed, NotoSansSemBold, NotoSansBold };
