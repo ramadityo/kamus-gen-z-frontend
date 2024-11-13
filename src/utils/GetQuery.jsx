eabdbad7e5fd940b3002d828cd9ef1cdb2db9f59
_userNull <ramzprivatexd@gmail.com>
2024-11-13 15:59:52 +0700

    remove 'use client' in page.js

--- src/utils/GetQuery.jsx
+++ src/utils/GetQuery.jsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 function GetQuery() {
     const searchParams = useSearchParams();
     const query = searchParams.get("q");
 
-    return <>{query}</>;
+    return (
+        <>
+            <Suspense fallback={<div>Loading...</div>}>{query}</Suspense>
+        </>
+    );
 }
 
 export default GetQuery;
